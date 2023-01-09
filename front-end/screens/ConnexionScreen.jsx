b3fda84abab80b8821293d3aa6d5ee0752ef2564
Tchessi <tchesspreza@yahoo.fr>
2023-01-09 10:47:50 +0100

    Front-end init

---
+++ front-end/screens/ConnexionScreen.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+class ConnexionScreen extends Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text></Text>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({})
+
+export default ConnexionScreen;
