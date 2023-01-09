e0ed682fcfc0fad57dc9abfec5eb61dd1af7a502
salim-ouari <91871755+salim-ouari@users.noreply.github.com>
2023-01-09 11:43:38 +0100

    merge dev ok

--- front-end/App.js
+++ front-end/App.js
@@ -1,26 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from "./components/Home";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* < <Image source={require('./assets/tiss.png')} />> */}
-      <Text style={styles.Text}>TissApp le chat du futur!</Text>
-      <StatusBar style="auto" />
+      <Home />
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
-  Text: {
-    color: '#fff',
-  },
   container: {
     flex: 1,
     backgroundColor: '#0F1828',
     alignItems: 'center',
     justifyContent: 'center',
-
   },
 });
