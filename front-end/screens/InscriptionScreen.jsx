6d2adabd1f1100adf87b8e3033d750b03ccf4a2d
ismailhas <ismail.hassan-ahmed@kalitics.com>
2023-01-13 12:15:22 +0100

    ismmo

--- front-end/screens/InscriptionScreen.jsx
+++ front-end/screens/InscriptionScreen.jsx
@@ -1,26 +1,14 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import {View, StyleSheet} from 'react-native';
-
-class InscriptionScreen extends Component {
-	render() {
-		return (
-			<View>
-				
-			</View>
-		);
-	}
-}
-
-const styles = StyleSheet.create({})
-=======
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, SafeAreaView, TextInput, View, TouchableHighlight, Text, Image } from 'react-native';
 import Styles from '../css/Styles'
 
 const InscriptionScreen = () => {
 	
-
+	const [firstname, setFirstname] = React.useState('');
+	const [lastname, setLastname] = React.useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [confirmpassword, setConfirmpassword] = useState()
 	return (
 		<SafeAreaView style={styles.container}>
 			<View style={styles.logoArea}>
@@ -31,34 +19,33 @@ const InscriptionScreen = () => {
 				style={Styles.input}
 				placeholder="Nom"
 				placeholderTextColor="#ffff"
-				keyboardType="name-family
-"
+				keyboardType="name-family" value={firstname} onChangeText={setFirstname}
 			/>
 			<TextInput
 				style={Styles.input}
 				placeholder="Prénom"
 				placeholderTextColor="#ffff"
-				keyboardType="name"
+				keyboardType="name"  value={lastname} onChangeText={setLastname}
 			/>
 			<TextInput
 				style={Styles.input}
 				placeholder="Email"
 				placeholderTextColor="#ffff"
 				keyboardType="email-address"
-				blurOnSubmit={true}
+				blurOnSubmit={true} value={email} onChangeText={setEmail}
 			/>
 			<TextInput
 				style={Styles.input}
 				placeholder="Mot de passe"
 				placeholderTextColor="#ffff"
 				keyboardType="password"
-				autoComplete="password-new"
+				autoComplete="password-new" value={password} onChangeText={setPassword}
 			/>
 			<TextInput
 				style={Styles.input}
 				placeholder="Confirmez votre mot de passe"
 				placeholderTextColor="#ffff"
-				keyboardType="password"
+				keyboardType="password" value={confirmpassword} onChangeText={setConfirmpassword}
 			/>
 			<View>
 				<TouchableHighlight
@@ -93,6 +80,5 @@ const styles = StyleSheet.create({
 	},
 	
 })
->>>>>>> tch-front
 
 export default InscriptionScreen;
