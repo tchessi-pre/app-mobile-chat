d33dbde505904ddb6e57fe665a5d637238ebbb9c
ismailhas <ismail.hassan-ahmed@kalitics.com>
2023-01-13 13:42:22 +0100

    packages

--- front-end/screens/InscriptionScreen.jsx
+++ front-end/screens/InscriptionScreen.jsx
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import {View, StyleSheet} from 'react-native';
-
-class InscriptionScreen extends Component {
-	render() {
-		return (
-			<View>
-				
-			</View>
-		);
-	}
-}
-
-const styles = StyleSheet.create({})
-=======
 import React from 'react';
 import { StyleSheet, SafeAreaView, TextInput, View, TouchableHighlight, Text, Image } from 'react-native';
 import Styles from '../css/Styles'
@@ -93,6 +77,5 @@ const styles = StyleSheet.create({
 	},
 	
 })
->>>>>>> tch-front
 
 export default InscriptionScreen;
