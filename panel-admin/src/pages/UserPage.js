2bf11afba9960d57d45fa88bb597f7c9584dd703
Tchessi  Pre <tchesspreza@yahoo.fr>
2023-04-30 17:50:29 +0200

    Add update user table

--- panel-admin/src/pages/UserPage.js
+++ panel-admin/src/pages/UserPage.js
@@ -35,8 +35,8 @@ import USERLIST from '../_mock/user';
 // ----------------------------------------------------------------------
 
 const TABLE_HEAD = [
-  { id: 'name', label: 'Nom', alignRight: false },
-  { id: 'company', label: 'Company', alignRight: false },
+  { id: 'name', label: 'Nom & Prénom', alignRight: false },
+  { id: 'email', label: 'Email', alignRight: false },
   { id: 'role', label: 'Role', alignRight: false },
   { id: 'isVerified', label: 'Verified', alignRight: false },
   { id: 'status', label: 'Status', alignRight: false },
