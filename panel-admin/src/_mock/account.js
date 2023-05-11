5c2dd48864dfa08b616ddf14fed2a73ea6e3e9f9
Tchessi  Pre <tchesspreza@yahoo.fr>
2023-05-11 11:08:47 +0200

    Add fix error console

--- panel-admin/src/_mock/account.js
+++
@@ -1,9 +0,0 @@
-// // ----------------------------------------------------------------------
-
-// const account = {
-//   displayName: 'Tchèssi PRE',
-//   email: 'tchess@test.fr',
-//   photoURL: '/assets/images/avatars/avatar_default.jpg',
-// };
-
-// export default account;
